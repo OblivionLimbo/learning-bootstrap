bfd633f8938ba4f3e840f88eaccd502d53c88065
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-03-20 14:54:01 +0100

    added files

---
+++ react-bootstrap-tutorial/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>
+          Test button
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
